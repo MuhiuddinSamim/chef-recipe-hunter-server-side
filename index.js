abca6bb6e1e69b4e54a37e35f7751c4e7340e69e
Muhiuddinsamim <Muhiuddinsamim007@gmail.com>
2023-05-04 10:36:26 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+const port =process.env.PORT || 5000;
+const cors = require('cors')
+const chef=require('./Data/Chef.json')
+
+app.use(cors())
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/chef', (req, res) => {
+  res.send(chef)
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
